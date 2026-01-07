27d625de3f6c68e4dfdeeb4852db73980052dc07
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-07 16:08:16 +0300

    Bubble_sort/bubble_sort.c

---
+++ Bubble_sort/build/CMakeFiles/bubble_sort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bubble_sort.dir/link.d"
+  "CMakeFiles/bubble_sort.dir/main.c.o"
+  "CMakeFiles/bubble_sort.dir/main.c.o.d"
+  "bubble_sort"
+  "bubble_sort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bubble_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
