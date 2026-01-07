85ef4b7749a726135d71a2e6576a2355165a4888
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-07 20:49:32 +0300

    Update main.c

--- Bubble_sort/Alphabets_Bubble_Sort/main.c
+++ Bubble_sort/Alphabets_Bubble_Sort/main.c
@@ -3,24 +3,24 @@
 
 int main() {
     char *words[] = {
-        "apple",
-        "carrots",
-        "oranges",
-        "eggs",
-        "milk",
-        "bananas",
-        "peach",
-        "cabbages"
+        "Apple",
+        "Carrots",
+        "Oranges",
+        "Eggs",
+        "Milk",
+        "Bananas",
+        "Peach",
+        "Cabbages"
     };
 
     int n = sizeof(words) / sizeof(words[0]);
 
-    printf("Original words:\n");
+    printf("Original Words:\n");
     print_strings(words, n);
 
     bubble_sort_strings(words, n);
 
-    printf("\nSorted words:\n");
+    printf("\nSorted Words:\n");
     print_strings(words, n);
 
     return 0;
