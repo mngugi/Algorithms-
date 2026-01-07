710486904839baecbfe286a71cdd4750951b16fe
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-07 17:55:05 +0300

    Update bubble_sort.c

--- Bubble_sort/Alphabets_Bubble_Sort/bubble_sort.c
+++ Bubble_sort/Alphabets_Bubble_Sort/bubble_sort.c
@@ -2,3 +2,23 @@
 #include <string.h>
 #include "bubble_sort.h"
 
+void bubble_sort_strings(char *arr[], int n);{
+
+    char t*temp;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strcmp(arr[j], arr[j + 1]) > 0) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void print_strings(char *arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", arr[i]);
+    }
+}
