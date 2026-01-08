f309b5f75fbbf5aa0b9f9b6f6c1405d644c69ac9
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-08 23:31:49 +0300

    .vscode/settings.json

--- Bubble_sort/Alphabets_Bubble_Sort/bubble_sort.c
+++ Bubble_sort/Alphabets_Bubble_Sort/bubble_sort.c
@@ -2,13 +2,14 @@
 #include <string.h>
 #include "bubble_sort.h"
 
-void bubble_sort_strings(char *arr[], int n){
+void bubble_sort_strings(char *arr[], int n){  // arr is an array of pointers
+    // Each element arr[i] points to a string in memory.
 
     char *temp;
 
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (strcmp(arr[j], arr[j + 1]) > 0) {
+            if (strcmp(arr[j], arr[j + 1]) > 0) { //strcmp(arr[j], arr[j+1]) compares the strings lexicographically.
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
