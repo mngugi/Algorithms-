4c8bdf71f2c6e3ce03cb269243ce96026a87d5ea
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-08 23:37:35 +0300

    Update bubble_sort.c

--- Bubble_sort/Alphabets_Bubble_Sort/bubble_sort.c
+++ Bubble_sort/Alphabets_Bubble_Sort/bubble_sort.c
@@ -10,7 +10,7 @@ void bubble_sort_strings(char *arr[], int n){  // arr is an array of pointers
     for (int i = 0; i < n - 1; i++) { //i counts passes. Each pass pushes the largest string to the end.
         for (int j = 0; j < n - i - 1; j++) { // j iterates over unsorted elements.
             //n - i - 1 avoids comparing elements that are already in place.
-            if (strcmp(arr[j], arr[j + 1]) > 0) { //strcmp(arr[j], arr[j+1]) compares the strings lexicographically.
+            if (strcmp(arr[j], arr[j + 1]) > 0) { //strcmp(arr[j], arr[j+1]) compares the strings lexicographically.If arr[j] is alphabetically after arr[j+1], swap them
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
