7f09318aa7b8de878b809826c2eca935d1b23451
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-15 16:47:59 +0300

    Update prog_main.c

--- Algoritms/Arrays/prog_main.c
+++ Algoritms/Arrays/prog_main.c
@@ -2,7 +2,7 @@
 #include "minlib.h"
 
 int main() {
-    int empty_arr[] = {12, 5, 7, 3, 19};
+    int empty_arr[] = {12, 5, 7, 2, 19};
     int size = sizeof(empty_arr) / sizeof(empty_arr[0]);
 
     int min = min_val(empty_arr, size);
