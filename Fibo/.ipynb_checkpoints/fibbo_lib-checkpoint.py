a7216753e0a6df5562a86621d415b2fdd408f9e6
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-17 16:10:52 +0300

    Fibo/fibbo_lib.py

--- Fibo/.ipynb_checkpoints/fibbo_lib-checkpoint.py
+++ Fibo/.ipynb_checkpoints/fibbo_lib-checkpoint.py
@@ -1,12 +1,13 @@
 def fibbo_function():
-    first_num =  0
+    first_num = 0
     second_num = 1
 
-    print(first_num)
-    print(second_num)
+    result = "0\n1\n"
 
     for i in range(10):
         new_fibonacci = first_num + second_num
-        print(new_fibonacci)
+        result += str(new_fibonacci) + "\n"
         first_num = second_num
         second_num = new_fibonacci
+
+    return result   # <-- THIS LINE IS REQUIRED
