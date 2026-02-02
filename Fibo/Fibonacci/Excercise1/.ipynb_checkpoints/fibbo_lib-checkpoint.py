7c6a6eb12ee82a300826facda4565b60676e508f
P.M.Ngugi <mngugi7@hotmail.com>
2026-02-02 22:38:25 +0300

    Fibo/Fibonacci/Excercise1/fibbo_prep.ipynb

---
+++ Fibo/Fibonacci/Excercise1/.ipynb_checkpoints/fibbo_lib-checkpoint.py
@@ -0,0 +1,13 @@
+def fibbo_function():
+    first_num = 0
+    second_num = 1
+
+    result = "0\n1\n"
+
+    for i in range(18):
+        new_fibonacci = first_num + second_num
+        result += str(new_fibonacci) + "\n"
+        first_num = second_num
+        second_num = new_fibonacci
+
+    return result   # <-- THIS LINE IS REQUIRED
