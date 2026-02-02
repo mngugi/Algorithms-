05fdfe59eba70a3b3129d817be45767bc6eadd83
P.M.Ngugi <mngugi7@hotmail.com>
2026-02-02 22:57:12 +0300

    Fibo/Fibonacci/Excercise1/main.ipynb

--- Fibo/Fibonacci/Excercise1/api.py
+++ Fibo/Fibonacci/Excercise1/api.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 @app.route("/fibbo")
 def fibbo_api():
-    return fibbo_lib.fibbo_function()
+    result = fibbo_lib.fibbo_function()  # your array
+    return jsonify(result)  # <-- convert array to JSON
+    #return fibbo_lib.fibbo_function()
 
 app.run()
